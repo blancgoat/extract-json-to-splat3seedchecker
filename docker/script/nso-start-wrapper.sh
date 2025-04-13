a720aef6bdd6e3c497b202bb815de90975aa1801
blancgoat <blancgoat@gmail.com>
2025-04-13 16:40:18 +0900

    job after, container shutdown

---
+++ docker/script/nso-start-wrapper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Waiting for device to be READY..."
+while true; do
+    STATUS=$(cat ~/device_status 2>/dev/null)
+    if [ "$STATUS" == "READY" ]; then
+        echo "Device is READY!"
+        break
+    fi
+    sleep 5
+done
+
+/app/docker-script/nso-start.sh
+
+# 스크립트 종료 코드 확인
+EXIT_CODE=$?
+
+# 종료 코드가 0이면 컨테이너 종료
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "Check ./output directory and get 'gear_####.json'. Upload to Splat3 Seed Checker!"
+    echo "Job done. Container shutdown..."
+    # 모든 프로세스에 SIGTERM 보내기
+    kill -TERM 1
+fi
+
+# 원래 스크립트의 종료 코드 유지
+exit $EXIT_CODE
--- docker/script/nso-start.sh
+++ docker/script/nso-start.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-echo "Waiting for device to be READY..."
-while true; do
-    STATUS=$(cat ~/device_status 2>/dev/null)
-    if [ "$STATUS" == "READY" ]; then
-        echo "Device is READY!"
-        break
-    fi
-    sleep 5
-done
-
 cd /apk
 adb install-multiple base.apk split_config.{x86_64,en,xhdpi}.apk
 adb shell am start -n com.nintendo.znca/com.nintendo.coral.ui.boot.BootActivity
 
+echo "Connect to http://localhost:6080"
 echo "Please login nintendo and join splatoon 3"
 
 while true; do
@@ -28,3 +19,4 @@ while true; do
 done
 
 echo "Script complete"
+exit 0
